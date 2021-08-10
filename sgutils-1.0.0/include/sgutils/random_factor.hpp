f8e5211dc93000433990a6022a4a24db4925cffc
pvthuyet <pvthuyet@gmail.com>
2021-08-10 11:46:27 +0700

    random

--- sgutils-1.0.0/include/sgutils/random_factor.hpp
+++ sgutils-1.0.0/include/sgutils/random_factor.hpp
@@ -8,6 +8,21 @@ class random_factor
 {
 public:
 	template<typename T> requires (std::integral<T> || std::floating_point<T>)
+		T random_number(T a, T b)
+	{
+		std::random_device rd;  //Will be used to obtain a seed for the random number engine
+		std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+		if constexpr (std::integral<T>) {
+			std::uniform_int_distribution<T> dis(a, b);
+			return dis(gen);
+		}
+		else {
+			std::uniform_real_distribution<T> dis(a, b);
+			return dis(gen);
+		}
+	}
+
+	template<typename T> requires (std::integral<T> || std::floating_point<T>)
 	T operator()(T a, T b)
 	{
 		std::random_device rd;  //Will be used to obtain a seed for the random number engine
